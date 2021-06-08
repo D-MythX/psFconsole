28b184732d969cb4b63cd01e6a43baac23194ab6
Osman Mohammed <85432917+Anonynusman@users.noreply.github.com>
2021-06-08 01:18:05 +0000

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Author: Anonynusman
+#Version: updated
+
+echo -e "\e[1;92m Setting up environment for \e[0m\e[1;94mANONYNUSMAN...\e[0m"
+sleep 0.5
+clear
+trap 'printf "\n";stop;exit 1' 2
+
+
+dependencies() {
+
+command -v php > /dev/null 2>&1 || { echo >&2 "php is required but it's not installed. Install it by using pkg install php. Aborting."; exit 1; }
+command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
+command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
+
+}
+
+rm -rf /$HOME/pSFconsole && cd && https://github.com/anonynusman/pSFconsole
+
